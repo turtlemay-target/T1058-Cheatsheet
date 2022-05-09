fc04dce6b9f44f5455f5c578834e8b62b9eb6642
Turtlemay <am@trtl.in>
2022-05-09 11:49:55 -0400

    Whitespace.

--- www/components/x-barcode-item.js
+++ www/components/x-barcode-item.js
@@ -2,37 +2,33 @@ import JsBarcode from "../web_modules/jsbarcode.js";
 
 import { getIconUrl } from "../icons.js";
 
-customElements.define(
-	"x-barcode-item",
+customElements.define("x-barcode-item", class extends HTMLElement {
+	connectedCallback() {
+		const template = document.getElementsByTagName("template").namedItem("barcode-item-template");
 
-	class extends HTMLElement {
-		connectedCallback() {
-			const template = document.getElementsByTagName("template").namedItem("barcode-item-template");
+		if (!template) return;
 
-			if (!template) return;
+		const name = this.getAttribute("name");
+		const value = this.getAttribute("value");
+		const iconUrl = getIconUrl(this.getAttribute("icon"));
 
-			const name = this.getAttribute("name");
-			const value = this.getAttribute("value");
-			const iconUrl = getIconUrl(this.getAttribute("icon"));
+		const f = document.importNode(template.content, true);
 
-			const f = document.importNode(template.content, true);
+		const nameEl = f.querySelector('[data-id="name"]');
+		const valueEl = f.querySelector('[data-id="value"]');
+		const canvasEl = f.querySelector("canvas");
 
-			const nameEl = f.querySelector('[data-id="name"]');
-			const valueEl = f.querySelector('[data-id="value"]');
-			const canvasEl = f.querySelector("canvas");
+		/** @type {HTMLElement | null} */
+		const bgEl = f.querySelector('[data-id="icon"]');
 
-			/** @type {HTMLElement | null} */
-			const bgEl = f.querySelector('[data-id="icon"]');
+		if (nameEl) nameEl.textContent = name;
+		if (valueEl) valueEl.textContent = value;
+		if (canvasEl && value) renderBarcode(canvasEl, value);
+		if (bgEl) bgEl.style.backgroundImage = `url("${iconUrl}")`;
 
-			if (nameEl) nameEl.textContent = name;
-			if (valueEl) valueEl.textContent = value;
-			if (canvasEl && value) renderBarcode(canvasEl, value);
-			if (bgEl) bgEl.style.backgroundImage = `url("${iconUrl}")`;
-
-			this.replaceWith(f);
-		}
+		this.replaceWith(f);
 	}
-);
+});
 
 /**
  * @param {HTMLCanvasElement} elem
